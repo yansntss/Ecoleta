f5392c4a04075f0c384a9a08a1009049eceb4a08
Yan Santos <yannsantos4@gmail.com>
2020-06-06 19:17:59 -0300

    add BD
    conhecendo a logica do bd

---
+++ src/database/db.js
@@ -0,0 +1,76 @@
+//importar a dependencai do sqlite
+//import sqlite3 from "sqlite3";
+const sqlite3 = require("sqlite3").verbose()
+
+//criar objeto que ira fazeroperaçoes no bd
+const db = new sqlite3.Database("./src/database/database.db")
+
+module.exports = db
+//utilizar o obejeto de banco de dados, para nossas operações
+
+db.serialize(()=>{
+    //com comandos SQL eu vou:
+
+    // 1 criar uma tabela
+//     db.run(`
+//         CREATE TABLE IF NOT EXISTS places (
+//             id INTEGER PRIMARY KEY AUTOINCREMENT,
+//             image TEXT,
+//             name TEXT,
+//             address TEXT,
+//             address2 TEXT,
+//             state TEXT,
+//             city TEXT,
+//             items TEXT
+//         );
+//     `)
+
+//     // 2 insserir dados na tabela
+
+//     const query = `
+//     INSERT INTO places (
+//         image,
+//         name,
+//         address,
+//         address2,
+//         state,
+//         city,
+//         items
+    
+//         ) VALUES(?,?,?,?,?,?,?);
+// `
+    
+//     const values = [
+//         "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60","Papersider", "Guilherme Gamballa, Jardin américa","Número 260", "Santa Catarina", "Rio do Sul", "Resíduos Eletrônicos, Lâmpada"
+//     ]
+
+//     function afterInsertData(err){
+//         if(err){
+//             return console.log(err)
+//         }
+//         console.log("cadastrado com sucesso")
+//         console.log(this)
+
+//     }
+
+//      db.run(query, values, afterInsertData)
+   
+    // 3 consultar os dados da tabela
+    // db.all(`SELECT name FROM places`, function(err, rows){
+    //     if(err){
+    //         return console.log(err)
+    //     }
+    //     console.log("aqui estao os seus resistros")
+    //     console.log(rows)
+    // })
+
+
+    // // 4 deletar dados da tabela
+
+    db.run(`DELETE FROM places WHERE ID = ?`,[3], function(err){
+        if(err){
+            return console.log(err)
+        }
+        console.log("resistro deletado com sucesso!")
+    })
+} )
--- src/server.js
+++ src/server.js
@@ -13,9 +13,11 @@
 //require > função para pegar o express e salvar na variavel "express" (parece redundante mas é isso msm que vc estar pensando yan do futuro)
 
 //executando a função express no server
-const express = require ("express")
+const express = require("express")
 const server = express()
 
+//pegar o banco de dados
+const db = require("./database/db")
 //configurar pasta publica
 server.use(express.static("public"))
 
@@ -23,7 +25,7 @@ server.use(express.static("public"))
 // utilizando o template engine
 
 // pedindo para o nodemodule o "nunjucks"
-const nunjucks = require ("nunjucks")
+const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
     express: server,
     noCache: true
@@ -45,17 +47,33 @@ nunjucks.configure("src/views", {
 // Lembrando que como estou rodando em um servidor, o "/" entende-se como home.
 
 // rota GET para a pagina home
-server.get ("/", (req, res) => {
-    return res.render("index.html", { title : "titulo"})
+server.get("/", (req, res) => {
+    return res.render("index.html", { title: "titulo" })
 })
 
 // rota GET para a pagina create-point
-server.get ("/create-point", (req, res) => {
+server.get("/create-point", (req, res) => {
     return res.render("create-point.html")
 })
 
-server.get ("/search", (req, res) => {
-    return res.render("search-results.html")
+server.get("/search", (req, res) => {
+
+    //pegar os dados do banco de dados
+
+    db.all(`SELECT * FROM places`, function (err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+        console.log("aqui estao os seus resistros")
+        console.log(rows)
+
+        const total = rows.length
+
+        // mostrar a pagina html com os dados do banco de dados
+        return res.render("search-results.html",{ places: rows, total})
+    })
+
+    
 })
 
 
