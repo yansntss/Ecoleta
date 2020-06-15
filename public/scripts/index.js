e37d5eb28a0bfcbeffb124b5ab9bc57412983db1
Yan Santos <yannsantos4@gmail.com>
2020-06-14 22:22:15 -0300

    modo noturno/diurno

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,6 +3,12 @@ const buttonSearch = document.querySelector("#page-home main a")
 const modal = document.querySelector("#modal")
 const close = document.querySelector("#modal .header a")
 
+const removeLogo =  document.querySelector("#page-home header img")
+
+
+
+
+
 buttonSearch.addEventListener("click", () => {
     modal.classList.remove("hide") 
 })
@@ -10,3 +16,8 @@ buttonSearch.addEventListener("click", () => {
 close.addEventListener("click", () => {
     modal.classList.add("hide")
 })
+
+function newFunction() {
+    if (removeLogo == 0)
+        console.log("imagem removida");
+}
